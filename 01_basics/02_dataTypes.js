9a9947a1ea7a67b6f924ba0c9740814df7d0bab4
Pranav-Ausekar <pranavausekar1@gmail.com>
2024-02-23 08:59:46 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+// These all are the primitive data types :
+
+// number = 123;
+// bigint = used to store larger number integer value 
+// string = " "
+// boolean = true/false 
+// null 
+// undefined 
+
+age = 24;
+fName = "pranav";
+valid = true;
+
+console.log(typeof age);
+console.log(typeof fName);
+console.log(typeof valid);
+
+
