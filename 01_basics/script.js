5c7d4f39740b4c37be94d4b62163da655eed40f0
Pranav-Ausekar <pranavausekar1@gmail.com>
2024-02-23 08:37:41 +0000

    file one complete

---
+++ 01_basics/script.js
@@ -0,0 +1,15 @@
+const accountId = 144553;
+let accountEmail = "pranav@gmail.com";
+var accountPassword = "pranav8808";
+accountCity = "pune";
+
+// accountId = 123123; you simply cannot change the value of const variable
+
+console.log(accountId);
+
+accountEmail = "ausekar@gmail.com";
+accountPassword = "ausekar8808";
+accountCity = "Bengluru";
+
+console.log([accountEmail,accountPassword,accountCity]);
+
