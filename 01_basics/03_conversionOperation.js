0c959f571c14e3669a55e6191550dee635ef8f9f
Pranav-Ausekar <pranavausekar1@gmail.com>
2024-03-12 10:51:03 +0000

    new changes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -6,3 +6,7 @@ console.log(typeof (num));
 let valueInNumber = Number(num);
 console.log(typeof (valueInNumber));
 console.log(valueInNumber);
+
+
+// "33" => 33        
+// "33abc" => NaN    ... Not a Number.
